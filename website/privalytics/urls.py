f0ded00cfab5e7fb9195cc0760c7a073f24c5a8b
Aquiles Carattino <aquiles@aquicarattino.com>
2019-02-06 17:03:00 +0100

    Added url route

--- website/privalytics/urls.py
+++ website/privalytics/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from tracker.views import NewTrackView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/tracker', NewTrackView.as_view(), name='New-Tracker'),
 ]
