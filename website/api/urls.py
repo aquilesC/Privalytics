2d05e5c9d61974c0a49f82f48ad8a2d467f53e93
Aquiles Carattino <aquiles@aquicarattino.com>
2019-02-26 07:16:59 +0100

    Changed the API permissions

--- website/api/urls.py
+++ website/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
 
 from api.views import TrackerView
 
 urlpatterns = [
-    path('tracker', csrf_exempt(TrackerView.as_view()), name='tracker-api'),
+    path('tracker', TrackerView.as_view(), name='tracker-api'),
 ]
--- website/api/views.py
+++ website/api/views.py
@@ -1,8 +1,9 @@
 import time
 
 from ipware.ip import get_real_ip
-from rest_framework.permissions import AllowAny
+
 from rest_framework import status
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,9 +11,12 @@ from api.serializer import TrackerSerializer
 from logs.models import TimeToStore
 
 
-class TrackerView(APIView):
-    permission_classes = (AllowAny, )
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+    def enforce_csrf(self, request):
+        return  # To not perform the csrf check previously happening
 
+class TrackerView(APIView):
+    authentication_classes = (CsrfExemptSessionAuthentication,)
     def post(self, request):
         t0 = time.time()
         serializer = TrackerSerializer(data=request.data)
@@ -26,3 +30,5 @@ class TrackerView(APIView):
             TimeToStore.objects.create(measured_time=(t1-t0), measured_type=TimeToStore.POST_RAW_TRACK)
             return Response({'message': 'OK'}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
